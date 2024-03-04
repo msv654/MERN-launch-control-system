b37cba04e49c8bd74250230010a8f55f2147e01b
msv-654 <654mstv@gmail.com>
2024-03-04 09:59:30 +0100

    MongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require('http');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = require('./app.js');
 const { loadPlanetsData } = require('./models/planets.model.js');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+    'mongodb+srv://nasa-api:PASSWORD@nasa-project.z0hrr9a.mongodb.net/?retryWrites=true&w=majority&appName=NASA-PROJECT';
+
 const server = http.createServer(app);
 
+const client = new MongoClient(MONGO_URL, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
 async function startServer() {
+    await client.connect();
+
     await loadPlanetsData();
 
     server.listen(PORT, () => {
