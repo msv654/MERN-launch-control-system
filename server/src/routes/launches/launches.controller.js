294fd40ec5f937887707a34362445b8571a9b54e
msv-654 <654mstv@gmail.com>
2024-03-06 08:44:26 +0100

    saving all launches to the database

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,8 +5,8 @@ const {
     abortLaunchById,
 } = require('../../models/launches.model.js');
 
-function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+    return res.status(200).json(await getAllLaunches());
 }
 
 function httpAddNewLaunch(req, res) {
